89ce7912ec445ebf2f39576922c271be679ca671
manish3551 <54400764+manish3551@users.noreply.github.com>
2022-01-13 17:25:57 +0530

    Responsive website 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function openMenu(){
-    document.getElementById("mobile-nav").style.right ="0px";
+    document.getElementById("mobile-nav").style.left ="0px";
 }
 function closeMenu(){
-    document.getElementById("mobile-nav").style.right="-280px"
+    document.getElementById("mobile-nav").style.left="-280px"
 }
